2ea6ac4b093698e7e7ca35ba5aeffeee85bec5d5
Jesse Smith <sosaucily@gmail.com>
2017-04-20 12:30:59 +0200

    remove unneeded reducer/selectors for unused containers from boilerplate

--- client/app/containers/App/selectors.js
+++ client/app/containers/App/selectors.js
@@ -4,28 +4,6 @@
 
 import { createSelector } from 'reselect';
 
-const selectGlobal = (state) => state.get('global');
-
-const makeSelectCurrentUser = () => createSelector(
-  selectGlobal,
-  (globalState) => globalState.get('currentUser')
-);
-
-const makeSelectLoading = () => createSelector(
-  selectGlobal,
-  (globalState) => globalState.get('loading')
-);
-
-const makeSelectError = () => createSelector(
-  selectGlobal,
-  (globalState) => globalState.get('error')
-);
-
-const makeSelectRepos = () => createSelector(
-  selectGlobal,
-  (globalState) => globalState.getIn(['userData', 'repositories'])
-);
-
 const makeSelectLocationState = () => {
   let prevRoutingState;
   let prevRoutingStateJS;
@@ -43,10 +21,5 @@ const makeSelectLocationState = () => {
 };
 
 export {
-  selectGlobal,
-  makeSelectCurrentUser,
-  makeSelectLoading,
-  makeSelectError,
-  makeSelectRepos,
   makeSelectLocationState,
 };
