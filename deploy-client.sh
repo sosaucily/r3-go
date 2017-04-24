f509a858153faca43a902a8d7a49ae2480a53440
Jesse Smith <sosaucily@gmail.com>
2017-04-24 22:02:52 +0200

    update deploy script for boilerplate

--- deploy-client.sh
+++ deploy-client.sh
@@ -21,11 +21,11 @@ then
 fi
 
 # Complile js in client directory
-(cd client && npm run deploy:prod)
+(cd client && npm run build)
 
 # Build the client code, and push the dist dir to the github pages system for hosting
-git add client/dist
+git add client/build
 git commit -m'compile js client for deploy'
 
-git push --force ghp `git subtree split --prefix client/dist HEAD`:master
+git push --force ghp `git subtree split --prefix client/build HEAD`:master
 git reset --hard head~1
