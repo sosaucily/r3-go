859619556745440a9b98b11b1d2d7e74f545107e
Jesse Smith <sosaucily@gmail.com>
2017-05-09 13:36:23 +0200

    update deploy script

--- deploy-client.sh
+++ deploy-client.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 #If all changed are committed, compile js and deploy to github static page
 
-DIRTYMESSAGE="dirty working directory: please resolve and try again"
+checkStatus () {
+  if [ $? != 0 ]
+  then
+    echo $1
+    exit 1
+  fi
+}
+
+DIRTYMESSAGE="Dirty working directory: please resolve and try again"
+BUILDMESSAGE="Something wrong with the build step: please resolve and try again"
 
 #check for modified files and error out the deploy
 git diff --exit-code > /dev/null
-
-if [ $? -eq 1 ]
-then
-  echo $DIRTYMESSAGE
-  exit 1
-fi
+checkStatus "$DIRTYMESSAGE"
 
 #check for modified cached files and error out the deploy
 git diff --cached --exit-code > /dev/null
-if [ $? -eq 1 ]
-then
-  echo $DIRTYMESSAGE
-  exit 1
-fi
+checkStatus "$DIRTYMESSAGE"
 
 # Complile js in client directory
-(cd client && npm run build)
+cd client
+git branch -D production 2> /dev/null
+git checkout -b production
+checkStatus "$BUILDMESSAGE"
 
-# Build the client code, and push the dist dir to the github pages system for hosting
-git add client/build
-git commit -m'compile js client for deploy'
+npm run build
+checkStatus "$BUILDMESSAGE"
+git add build
+git commit --amend -C HEAD --no-verify
+git push --force origin production
 
-git push --force ghp `git subtree split --prefix client/build HEAD`:master
-git reset --hard head~1
+echo "FINISHED!"
