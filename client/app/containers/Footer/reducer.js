de457cbae794ee88f7ab40b572593c86a501c528
Jesse Smith <sosaucily@gmail.com>
2017-05-22 15:23:50 +0200

    fix error with fetching new github sha

--- client/app/containers/Footer/reducer.js
+++ client/app/containers/Footer/reducer.js
@@ -5,7 +5,7 @@ import { GET_SHA_SUCCESS } from './constants';
 const initialState = fromJS({});
 
 const ACTION_HANDLERS = {
-  [GET_SHA_SUCCESS]: (state, { payload }) => state.set('sha', payload[0].object.sha),
+  [GET_SHA_SUCCESS]: (state, { payload }) => state.set('sha', payload.object.sha),
 };
 
 export default function footerReducer(state = initialState, action) {
